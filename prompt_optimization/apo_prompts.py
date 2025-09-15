6b23e3098b2ebd551f51dafdb435e3059e0345a8
Johnson Kao <johnson0213@g.ucla.edu>
2025-09-15 13:24:14 -0700

    Update apo_prompts.py

--- prompt_optimization/apo_prompts.py
+++ prompt_optimization/apo_prompts.py
@@ -7,3 +7,9 @@ BRACS_DCIS_IC = '''I'm trying to write a prompt used to generate description for
 BRACS_multi = '''I'm trying to write a prompt used to generate description for the given image, where the generated description can enhance the image zero-shot classification of breast tumor in the image.
         The seven types of breast tumors are: A. Normal, B. Ductal Carcinoma In Situ, C. Invasive Carcinoma.
         '''
+BACH = """I'm trying to write a prompt used to generate description for the given image, where the generated description can enhance the image zero-shot classification of breast tumor in the breast cancer histology images.
+        The two types of breast tumors are: A. Normal, B. Invasive."""
+SICAPv2 = """I'm trying to write a prompt used to generate description for the given image, where the generated description can enhance the image zero-shot classification of whether the tissue is non-cancerous or contains prostate cancer in the prostate tissue images.
+        The two types of categories are: A. Non-cancerous, B. Prostate cancer."""
+Gleason = """I'm trying to write a prompt used to generate description for the given image, where the generated description can enhance the image zero-shot classification of whether the tissue is non-cancerous or contains prostate cancer in the prostate tissue images.
+        The two types of categories are: A. Non-cancerous, B. Prostate cancer."""
